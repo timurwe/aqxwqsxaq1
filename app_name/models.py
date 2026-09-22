e8fc21f0ed64331fcdae71ac7154b42b6e87e372
Artem030508 <arp27849@gmail.com>
2026-09-22 20:25:05 +0500

    1

---
+++ app_name/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+class Grade(models.Model):
+    number = models.PositiveIntegerField()
+
+class Textbook(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200) 
+    author = models.CharField(max_length=100)
+
+class Task(models.Model):
+    textbook = models.ForeignKey(Textbook, on_delete=models.CASCADE)
+    number = models.CharField(max_length=50) 
+    solution_text = models.TextField(blank=True, null=True)
+    solution_image = models.ImageField(upload_to='solutions/', blank=True, null=True)
